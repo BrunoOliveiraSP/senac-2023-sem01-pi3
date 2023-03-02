60d35448e0553921f81f149f38e476d77f568453
Bruno O <brunodeoliveira.22.10@gmail.com>
2023-03-02 20:15:07 -0300

    okkk tudo lindo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Aluno from './Aluno';
 
-function App() {
+import { Link } from 'react-router-dom';
+
+export default function App() {
+
   return (
-    <div className="App">
+    <div className="pagina-app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='titulo'> Alunos </h1>
+        <Link to='/contato'>Ir para contato</Link>
+        <Aluno nome="Bruno de Oliveira" formacao="Analista de Sistemas" />
+        <Aluno nome="João José" formacao="Engenheiro de Software" />
       </header>
+      
     </div>
   );
+
 }
 
-export default App;
---
+++ src/Contato.js
@@ -0,0 +1,12 @@
+
+
+export default function Contato() {
+
+  return (
+    <div>
+      <h1> Contato </h1>
+
+      <p> Oieee</p>
+    </div>
+  )
+}
